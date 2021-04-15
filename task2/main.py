49a7b560f67f7f51a9e3f699f1aced5119e4b4b0
wenkf <wenkf@student.ethz.ch>
2021-04-15 19:52:02 +0200

    Added Separate test set (other part of train set). Added score calculation. Changed Fitting for vital signs to linear regression, much better result but still quite bad

--- task2/main.py
+++ task2/main.py
@@ -20,8 +20,16 @@ train_features = pd.read_csv('data/train_features.csv')
 train_labels = pd.read_csv('data/train_labels.csv')
 test_features = pd.read_csv('data/test_features.csv')
 
-train_features = train_features.head(100*12)
-train_labels = train_labels.head(100)
+train_begin = 0
+train_end = 999
+test_begin = 1000
+test_end = 1999
+
+train_X = train_features[train_begin*12:train_end*12]
+train_y = train_labels[train_begin:train_end]
+
+test_X = train_features[test_begin*12:test_end*12]
+test_y = train_labels[test_begin:test_end]
 
 medical_tests = ['LABEL_BaseExcess', 'LABEL_Fibrinogen', 'LABEL_AST',
                  'LABEL_Alkalinephos', 'LABEL_Bilirubin_total', 'LABEL_Lactate',
@@ -58,14 +66,24 @@ add_indicator = False
 print("Start imputing stuff")
 
 imputeMean = SimpleImputer(missing_values=np.nan, strategy='mean', add_indicator=add_indicator)
-train_imputed = pd.DataFrame(imputeMean.fit_transform(train_features), columns=train_features.columns)
+if add_indicator:
+    train_X_imputed = pd.DataFrame(imputeMean.fit_transform(train_X), columns=train_X.columns.append('imputed_' + train_X.columns[train_X.isna().sum() > 0]))
+    test_X_imputed = pd.DataFrame(imputeMean.fit_transform(test_X), columns=test_X.columns.append('imputed_' + test_X.columns[test_X.isna().sum() > 0]))
+else:
+    train_X_imputed = pd.DataFrame(imputeMean.fit_transform(train_X), columns=train_X.columns)
+    test_X_imputed = pd.DataFrame(imputeMean.fit_transform(test_X), columns=test_X.columns)
 
 # TODO: Filter out age/time columns
 
-train_features_flat = train_imputed.set_index('pid').groupby(level=0) \
+train_X_flat = train_X_imputed.set_index('pid').groupby(level=0) \
     .apply(lambda df: df.reset_index(drop=True)) \
     .unstack().sort_index(axis=1, level=1)
-train_features_flat.columns = ['{}{}'.format(x[0], int(x[1]) + 1) for x in train_features_flat.columns]
+train_X_flat.columns = ['{}{}'.format(x[0], int(x[1]) + 1) for x in train_X_flat.columns]
+
+test_X_flat = test_X_imputed.set_index('pid').groupby(level=0) \
+    .apply(lambda df: df.reset_index(drop=True)) \
+    .unstack().sort_index(axis=1, level=1)
+test_X_flat.columns = ['{}{}'.format(x[0], int(x[1]) + 1) for x in test_X_flat.columns]
 
 # train_features.groupby(['pid']).mean()
 # pid AAA BBB CCC DDD                 pid AAA BBB CCC DDD AAA BBB CCC DDD AAA BBB CCC DDD
@@ -76,18 +94,19 @@ train_features_flat.columns = ['{}{}'.format(x[0], int(x[1]) + 1) for x in train
 print("done!")
 
 # Train classifiers to predict ordered medical tests
-X = train_features_flat
+X = train_X_flat
 medical_test_scores = []
 
 for medical_test in medical_tests:
     print("start SVM fitting for " + medical_test)
-    y = train_labels[medical_test]
+    y = train_y[medical_test]
+    y_test = test_y[medical_test]
     clf = svm.SVC(kernel='rbf', cache_size=1000)
-    #clf.probability = True
+    clf.probability = True
     clf.fit(X, y)
     #probabilities = clf.predict_proba(X)  # values between [0,1]
-    medical_test_scores.append([clf.score(X, y),
-                                roc_auc_score(y, clf.decision_function(X))])
+    medical_test_scores.append([clf.score(test_X_flat, y_test),
+                                roc_auc_score(y_test, clf.decision_function(test_X_flat))])
 
 for i, test in enumerate(medical_tests):
     print("\n" + test + "\n average accuracy: " + str(medical_test_scores[i][0]) +
@@ -96,20 +115,22 @@ for i, test in enumerate(medical_tests):
 
 # Train classifier to predict sepsis event
 print("start svm fitting for sepsis prediction")
-X = train_features_flat
-y = train_labels['LABEL_Sepsis']
-clf = svm.SVC(kernel='rbf', cache_size=1000)
+X = train_X_flat
+y = train_y['LABEL_Sepsis']
+y_test = test_y['LABEL_Sepsis']
+clf = svm.SVC(kernel='linear', cache_size=1000)
 clf.fit(X, y)
-print("\nSepsis prediction\n average accuracy: " + str(clf.score(X, y)) +
-      "\n roc_auc score: " + str(roc_auc_score(y, clf.decision_function(X))))
+print("\nSepsis prediction\n average accuracy: " + str(clf.score(test_X_flat, y_test)) +
+      "\n roc_auc score: " + str(roc_auc_score(y_test, clf.decision_function(test_X_flat))))
 
 # Train regressor to predict mean value of vital signs
-print("start svr fitting for vital signs")
-X = train_features_flat
-y = train_labels[['LABEL_RRate']] #, 'LABEL_ABPm', 'LABEL_SpO2', 'LABEL_Heartrate']]
-reg = MultiOutputRegressor(svm.SVR(kernel='rbf'))  # lots of parameters that could be set here
+print("\nstart linear fitting for vital signs")
+X = train_X_flat
+y = train_y[['LABEL_RRate', 'LABEL_ABPm', 'LABEL_SpO2', 'LABEL_Heartrate']]
+y_test = test_y[['LABEL_RRate', 'LABEL_ABPm', 'LABEL_SpO2', 'LABEL_Heartrate']]
+reg = MultiOutputRegressor(linear_model.LinearRegression())  # lots of parameters that could be set here
 reg.fit(X, y)
-print("\nVital signs prediction\n R2 score: " + str(r2_score(reg.predict(X), y)))
+print("\nVital signs prediction\n R2 score: " + str(r2_score(reg.predict(test_X_flat), y_test)))
 
 # # Evaluate strategies
 # results = list()
@@ -129,6 +150,11 @@ print("\nVital signs prediction\n R2 score: " + str(r2_score(reg.predict(X), y))
 #plt.boxplot(results, labels=strategies, showmeans=True)
 #plt.show()
 
+task1_score = np.mean(np.array(medical_test_scores)[:, 1])
+task2_score = roc_auc_score(test_y['LABEL_Sepsis'], clf.decision_function(test_X_flat))
+task3_score = np.mean([0.5 + 0.5 * np.maximum(0, r2_score(reg.predict(test_X_flat), test_y[['LABEL_RRate', 'LABEL_ABPm', 'LABEL_SpO2', 'LABEL_Heartrate']]))])
 
+total_score = np.mean([task1_score, task2_score, task3_score])
 
-
+print("\nScores:\n\nTask 1: " + str(task1_score) + "\nTask 2: " + str(task2_score) + "\nTask 3: " + str(task3_score) + "\n\nTotal Score: " + str(total_score))
+print("\nBaseline: " + str(0.713853457215))
