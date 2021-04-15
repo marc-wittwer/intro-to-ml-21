4e879a8d253825168e5511abdb35039b96924a7a
wenkf <wenkf@student.ethz.ch>
2021-04-15 12:35:07 +0200

    Task2 beginning

--- task2/main.py
+++ task2/main.py
@@ -3,8 +3,13 @@ import numpy as np
 from scipy import stats
 from sklearn import linear_model
 from sklearn import metrics
-from sklearn.model_selection import KFold
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold, RepeatedStratifiedKFold
+from sklearn.ensemble import RandomForestClassifier
 import matplotlib.pyplot as plt
+from sklearn import svm
 
 #
 # read data (as a pd.DataFrame)
@@ -13,13 +18,85 @@ train_features = pd.read_csv('data/train_features.csv')
 train_labels = pd.read_csv('data/train_labels.csv')
 test_features = pd.read_csv('data/test_features.csv')
 
+train_features = train_features.head(100*12)
+train_labels = train_labels.head(100)
 # Missing Features
+# ------ indicator flags (dummy values) für imputed values
+add_indicator = False
 
+# ------ mean über ganze spalte
+# imputeMean = SimpleImputer(missing_values=np.nan, strategy='mean', add_indicator=add_indicator)
+# train_imputeMean = pd.DataFrame(imputeMean.fit_transform(train_features), columns=train_features.columns)
+# ------ 0 setzen
+# imputeZero = SimpleImputer(missing_values=np.nan, strategy='constant', fill_value=0, add_indicator=add_indicator)
+# train_imputeZero = pd.DataFrame(imputeZero.fit_transform(train_features), columns=train_features.columns)
 
+# ------ median über spalte
+# imputeMedian = SimpleImputer(missing_values=np.nan, strategy='median', add_indicator=add_indicator)
+# train_imputeMedian = pd.DataFrame(imputeMedian.fit_transform(train_features), columns=train_features.columns)
+
+# (- häufigster wert)
+# imputeMostFrequent = SimpleImputer(missing_values=np.nan, strategy='most_frequent', add_indicator=add_indicator)
+# train_imputeMostFreq = pd.DataFrame(imputeMostFrequent.fit_transform(train_features), columns=train_features.columns)
 
 
 
 # Imbalanced Classification
+
+
+
+
+
+print("Start imputing stuff")
+
+imputeMean = SimpleImputer(missing_values=np.nan, strategy='mean', add_indicator=add_indicator)
+train_imputed = pd.DataFrame(imputeMean.fit_transform(train_features), columns=train_features.columns)
+
+# TODO: Filter out age/time columns
+
+train_features_flat = train_imputed.set_index('pid').groupby(level=0) \
+    .apply(lambda df: df.reset_index(drop=True)) \
+    .unstack().sort_index(axis=1, level=1)
+train_features_flat.columns = ['{}{}'.format(x[0], int(x[1]) + 1) for x in train_features_flat.columns]
+
+print("done!")
+
+X = train_features_flat
+y = train_labels['LABEL_BaseExcess']
+
+
+# train_features.groupby(['pid']).mean()
+# pid AAA BBB CCC DDD                 pid AAA BBB CCC DDD AAA BBB CCC DDD AAA BBB CCC DDD
+# 1   0   0   0   0                   1   0   0   0   0   0   0   0   0   0   0   0   0
+# 1   0   0   0   0     reshape to
+# 1   0   0   0   0
+
+print("start SVM fitting")
+clf = svm.SVC(kernel='rbf', cache_size=1000)
+clf.probability = True
+clf.fit(X, y)
+probabilities = clf.predict_proba(X)  # values between [0,1]
+
+print(probabilities)
+
+# # Evaluate strategies
+# results = list()
+# strategies = ['mean', 'median', 'most_frequent', 'constant']
+#for s in strategies:
+#    # create the modeling pipeline
+#    pipeline = Pipeline(steps=[('i', SimpleImputer(strategy=s)), ('m', RandomForestClassifier())]) # Replace Classifier by model
+#    # evaluate the model
+#    cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=42)
+#    scores = cross_val_score(pipeline, X, y, scoring='accuracy', cv=cv, n_jobs=-1)
+#    # store results
+#    results.append(scores)
+#    print('>%s %.3f (%.3f)' % (s, np.mean(scores), np.std(scores)))
+
+
+# plot model performance for comparison
+#plt.boxplot(results, labels=strategies, showmeans=True)
+#plt.show()
+
 
 
 
