c10e9809dfe7e3bffce21679f7f606c38ae6753a
wenkf <wenkf@student.ethz.ch>
2021-04-15 09:28:05 +0200

    Added task2 main

---
+++ task2/main.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+from scipy import stats
+from sklearn import linear_model
+from sklearn import metrics
+from sklearn.model_selection import KFold
+import matplotlib.pyplot as plt
+
+#
+# read data (as a pd.DataFrame)
+#
+train_features = pd.read_csv('data/train_features.csv')
+train_labels = pd.read_csv('data/train_labels.csv')
+test_features = pd.read_csv('data/test_features.csv')
+
+# Missing Features
+
+
+
+
+
+# Imbalanced Classification
+
+
+
