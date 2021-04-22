59946e93b57ec002a7ea537f930542b6f97e72b0
Marc Wittwer <mwittwer@ethz.ch>
2021-04-22 20:12:33 +0200

    Add hand-in-files folder

---
+++ task2/hand-in-files/feature_extraction.py
@@ -0,0 +1,119 @@
+import pandas as pd
+from multiprocessing import Pool
+from functools import partial
+
+tests = ['EtCO2', 'PTT', 'BUN', 'Lactate', 'Temp', 'Hgb', 'HCO3', 'BaseExcess', 'RRate', 'Fibrinogen',
+         'Phosphate', 'WBC', 'Creatinine', 'PaCO2', 'AST', 'FiO2', 'Platelets', 'SaO2', 'Glucose', 'ABPm',
+         'Magnesium', 'Potassium', 'ABPd', 'Calcium', 'Alkalinephos', 'SpO2', 'Bilirubin_direct', 'Chloride',
+         'Hct', 'Heartrate', 'Bilirubin_total', 'TroponinI', 'ABPs', 'pH']
+appendixes = ['_median', '_std', '_min', '_max', '_nTests']
+columns_only_mean = ['Time', 'Age']
+
+
+def extract_features(features):
+    # Extract features per patient
+    # mean
+    features_flat = features.groupby(['pid']).mean()
+    # median
+    features_flat = features_flat.join(features.groupby(['pid']).median(), rsuffix=appendixes[0])
+    # std
+    features_flat = features_flat.join(features.groupby(['pid']).std(), rsuffix=appendixes[1])
+    # min
+    features_flat = features_flat.join(features.groupby(['pid']).min(), rsuffix=appendixes[2])
+    # max
+    features_flat = features_flat.join(features.groupby(['pid']).max(), rsuffix=appendixes[3])
+    # num Tests
+    features_flat = features_flat.join(features.notna().groupby(features['pid']).sum().drop('pid', axis=1), rsuffix=appendixes[4])
+
+    return features_flat
+
+
+def interpolate_patient(pid, features):
+    pat_features = features.loc[features['pid'] == pid].interpolate(method='linear', axis=0, limit_direction='both')
+    pat_features.fillna(0, inplace=True)
+
+    features_flat = pat_features.set_index('pid').groupby(level=0).apply(
+        lambda df: df.reset_index(drop=True)).unstack()
+    features_flat.columns = ['{}{}'.format(x[0], int(x[1]) + 1) for x in features_flat.columns]
+
+    return features_flat
+
+
+def interpolate_per_patient(features):
+    print("Start interpolate per patient:")
+
+    # Parallelization
+    pool = Pool(n_jobs)
+    interpolated_features = pd.concat(pool.map(partial(interpolate_patient, features=features), features['pid'].unique()))
+
+    print("Finished interpolation per patient")
+
+    return interpolated_features
+
+
+def drop_columns(features, drop_time_columns, use_features):
+    if use_features:
+        features_dropped = features
+        for appendix in appendixes:
+            for col in columns_only_mean:
+                features_dropped = features_dropped.drop(col+appendix, axis=1)
+        if drop_time_columns:
+            features_dropped = features_dropped.drop('Time', axis=1)
+    else:
+        # Drop Age/Time duplicates columns
+        time_column_names = ['Time2', 'Time3', 'Time4', 'Time5', 'Time6', 'Time7', 'Time8', 'Time9', 'Time10',
+                             'Time11', 'Time12']
+        age_column_names = ['Age2', 'Age3', 'Age4', 'Age5', 'Age6', 'Age7', 'Age8', 'Age9', 'Age10', 'Age11', 'Age12']
+        columns_to_drop = time_column_names + age_column_names if drop_time_columns else age_column_names
+        features_dropped = features.drop(columns_to_drop, axis=1)
+
+    return features_dropped
+
+
+if __name__ == "__main__":
+    #
+    # read data (as a pd.DataFrame)
+    #
+
+    train_features = pd.read_csv('data/train_features.csv')
+    train_labels = pd.read_csv('data/train_labels.csv', index_col='pid')
+    test_features = pd.read_csv('data/test_features.csv')
+
+    # how many cores to use, adapt to availability
+    n_jobs = 3
+
+    # use features (mean, std implemented) instead of concatenating
+    use_features = True
+
+    # If time is to be dropped
+    drop_time = False
+
+    if use_features:
+        #
+        # Extract features per patient
+        #
+        features_interpolated = extract_features(train_features)
+        test_features_interpolated = extract_features(test_features)
+    else:
+        #
+        # Replace nans with interpolation per patient (or 0 if no value for this patient at all)
+        #
+        features_interpolated = interpolate_per_patient(train_features)
+        test_features_interpolated = interpolate_per_patient(test_features)
+
+    train_features = drop_columns(features_interpolated, drop_time, use_features)
+    test_features = drop_columns(test_features_interpolated, drop_time, use_features)
+
+    train_features.sort_index(axis=0, ascending=True, inplace=True)
+    train_labels.sort_index(axis=0, ascending=True, inplace=True)
+    test_features.sort_index(axis=0, ascending=True, inplace=True)
+
+    if use_features:
+        train_features.to_csv('data/train_extracted_features.csv', index=True)
+        train_labels.to_csv('data/train_extracted_labels.csv', index=True)
+        test_features.to_csv('data/test_extracted_features.csv', index=True)
+    else:
+        train_features.to_csv('data/train_features_interpolated.csv', index=True)
+        train_labels.to_csv('data/train_labels_interpolated.csv', index=True)
+        test_features.to_csv('data/test_features_interpolated.csv', index=True)
+    print("interpolated datasets have been generated.")
---
+++ task2/hand-in-files/main_final.py
@@ -0,0 +1,79 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn import svm
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.feature_selection import SelectKBest, mutual_info_classif
+from sklearn.linear_model import Lasso
+import time
+
+n_jobs = 12  # number of CPU cores to use
+np.random.seed(42)  # global random seed to get reproducible results
+
+train_features = pd.read_csv('data/train_extracted_features.csv', index_col='pid')
+train_labels = pd.read_csv('data/train_extracted_labels.csv', index_col='pid')
+test_features = pd.read_csv('data/test_extracted_features.csv', index_col='pid')
+
+
+test_predictions = []  # predictions on test set will be stored here
+
+
+def timer(start, end):
+    hours, rem = divmod(end-start, 3600)
+    minutes, seconds = divmod(rem, 60)
+    return "{:0>2}:{:0>2}:{:05.2f}".format(int(hours), int(minutes), seconds)
+
+
+start_time = time.time()
+t = start_time
+
+# Subtask 1 + 2
+medical_tests = ['LABEL_BaseExcess', 'LABEL_Fibrinogen', 'LABEL_AST',
+                 'LABEL_Alkalinephos', 'LABEL_Bilirubin_total', 'LABEL_Lactate',
+                 'LABEL_TroponinI', 'LABEL_SaO2', 'LABEL_Bilirubin_direct',
+                 'LABEL_EtCO2', 'LABEL_Sepsis']
+
+lambdas_clf = [1, 0.1, 1, 0.1, 1, 0.1, 0.1, 1, 0.1, 1, 0.15]  # regularization parameters
+
+for i, test_label in enumerate(medical_tests):
+    print("\nFitting " + test_label + " ...")
+    clf = Pipeline(steps=[('imputer', SimpleImputer(strategy='mean')),
+                          ('scaler', StandardScaler()),
+                          ('sel', SelectKBest(mutual_info_classif, k=100)),
+                          ('svc', svm.SVC(class_weight='balanced', C=lambdas_clf[i], probability=True))])
+
+    clf.fit(train_features, train_labels[test_label])
+    test_predictions.append(clf.predict_proba(test_features)[:, 1])
+    print("  " + str(i) + "/" + str(len(medical_tests)) + " completed")
+    print("  Time elapsed (this test / total) ---> " + timer(t, time.time()) + " / " + timer(start_time, time.time()))
+    t = time.time()
+
+
+# Subtask 3
+vital_signs = ['LABEL_RRate', 'LABEL_ABPm', 'LABEL_SpO2', 'LABEL_Heartrate']
+lambdas_reg = [0.32, 1.2, 0.1, 0.11]
+
+for i, sign in enumerate(vital_signs):
+    print("\nFitting " + sign + " ...")
+    reg = Pipeline(steps=[('imputer', SimpleImputer(strategy='mean')),
+                          ('scaler', StandardScaler()),
+                          ('reg', Lasso(alpha=lambdas_reg[i]))])
+
+    reg.fit(train_features, train_labels[sign])
+    test_predictions.append(reg.predict(test_features))
+    print("  Time elapsed (this test / total) [s]: " + timer(t, time.time()) + " / " + timer(start_time, time.time()))
+    t = time.time()
+
+
+# generate output
+print("\nStoring result ...")
+sol = pd.DataFrame(np.stack([test_predictions[i] for i in range(len(test_predictions))], axis=-1),
+                   index=test_features.index)
+sol.columns = medical_tests + vital_signs
+sol.to_csv('data/prediction.csv', index=True, float_format='%.3f')
+print("Done!")
+
+
+
+
