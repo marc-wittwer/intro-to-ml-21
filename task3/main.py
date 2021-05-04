e390713840ddb00419080d699d4007170b999f45
wenkf <wenkf@student.ethz.ch>
2021-05-04 12:26:09 +0200

    HistGradientBoostingClassifier first upload (score 0.9)

--- task3/main.py
+++ task3/main.py
@@ -3,7 +3,11 @@ import numpy as np
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import GridSearchCV
 from sklearn import svm
+from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import f1_score
+
+from sklearn.experimental import enable_hist_gradient_boosting
+from sklearn.ensemble import HistGradientBoostingClassifier
 
 #
 # read data (as a pd.DataFrame)
@@ -11,17 +15,23 @@ from sklearn.metrics import f1_score
 train_features = pd.read_csv('data/train_encoded_features.csv')
 train_labels = pd.read_csv('data/train_labels.csv')
 
-train_features = train_features[:5000]
-train_labels = train_labels[:5000]
+# train_features = train_features[:20000]
+# train_labels = train_labels[:20000]
 
 test_features = pd.read_csv('data/test_encoded_features.csv')
 
 n_jobs = 7
+np.random.seed(42)
 
 grid_search_params = [
-    {'clf': svm.SVC(),
-     'params': {'clf__C': [0.1, 1, 100],
-                'clf__class_weight': ['balanced', None]}},
+    # {'clf': svm.SVC(),
+    #  'params': {'clf__C': [0.1, 1, 100, 1000],
+    #             'clf__class_weight': ['balanced']}},
+    {'clf': HistGradientBoostingClassifier(max_iter=1000, random_state=42),
+     'params': {'clf__l2_regularization': [0.4, 0.5, 0.6],
+                'clf__max_leaf_nodes': [50, 100, 150],
+                'clf__min_samples_leaf': [1, 2, 5],
+                'clf__categorical_features': [[0, 1, 2, 3]]}}
 
     ]  # name is important
 
@@ -35,7 +45,7 @@ for params in grid_search_params:
 
     grid_search.fit(train_features, train_labels["Active"])
 
-    results.append(pd.DataFrame({'classifier': params['clf'], 'param': grid_search.cv_results_["params"],
+    results.append(pd.DataFrame({'classifier': str(params['clf']), 'param': grid_search.cv_results_["params"],
                                 'mean_score': grid_search.cv_results_["mean_test_score"],
                                'fit_time': grid_search.cv_results_["mean_fit_time"]}))
 
@@ -48,10 +58,9 @@ with pd.option_context('display.max_rows', None, 'display.max_columns', None):
     for i, res in enumerate(results):
         print(res)
 
-    # Predict labels for test data
-    # best_clf = grid_search.best_estimator_
-    # y_test_predictions = best_clf.predict(test_features)
-#
+# Predict labels for test data with best classifier (only one classifier -> only one best_estimator)
+best_clf = best_estimators[0]
+y_test_predictions = pd.DataFrame(best_clf.predict(test_features))
 
 # #  Save predictions to file
-# y_test_predictions.to_csv('data/predictions.csv', index=False)
+y_test_predictions.to_csv('data/predictions.csv', index=False, header=False)
