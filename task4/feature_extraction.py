80281e8f1c55ad1cb9e57df13ce196d733bcd1af
David Dubach <david.dubach96@gmail.com>
2021-05-19 11:37:55 +0200

    make multiprocessing windowsable again; add reproducibility test for feature extraction

--- task4/feature_extraction.py
+++ task4/feature_extraction.py
@@ -6,60 +6,90 @@ import torch
 import numpy as np
 import matplotlib.pyplot as plt
 import time
+from itertools import repeat
 import multiprocessing
 
-np.random.seed(42)
-torch.manual_seed(42)
-
-
-# Data augmentation and normalization for training
-# Just normalization for validation
-data_transforms = {
-    'train': transforms.Compose([
-        transforms.RandomResizedCrop(224),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ]),
-    'val': transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ]),
-}
-
-
-data_dir = 'data'
-image_dataset = datasets.ImageFolder(os.path.join(data_dir), data_transforms['train'])
-dataloader = torch.utils.data.DataLoader(image_dataset, batch_size=1, shuffle=False, num_workers=2)
-
-dataset_size = len(image_dataset)
-class_name = image_dataset.classes
-
+# keep this stuff global
 extraction_net = models.resnet101(pretrained=True)
 
 
-def processImage(tuple):
-    i, data = tuple
+def process_image(img_tuple, features):
+    i, data = img_tuple
     print(i)
     out = extraction_net(data[0])
-    features_dict[i] = out.data.numpy()[0]
+    features[i] = out.data.numpy()[0]
 
 
-features_dict = multiprocessing.Manager().dict()
+def extract_features():
+    # Data augmentation and normalization for training
+    # Just normalization for validation
+    data_transforms = {
+        'train': transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+        'val': transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+    }
 
-with multiprocessing.Pool(multiprocessing.cpu_count() - 1) as P:
-    P.map(processImage, enumerate(dataloader), chunksize=1)
-    P.close()
-    P.join()
+    data_dir = 'data'
+    image_dataset = datasets.ImageFolder(os.path.join(data_dir), data_transforms['val'])
+    dataloader = torch.utils.data.DataLoader(image_dataset, batch_size=1, shuffle=False, num_workers=0)
 
-final_features = []
-for i in range(len(features_dict)):
-    final_features.append(features_dict[i])
+    dataset_size = len(image_dataset)
+    class_name = image_dataset.classes
 
-df = pd.DataFrame(final_features)
-df.to_csv('data/train_image_features.csv', index=True, header=False)
+    features_dict = multiprocessing.Manager().dict()
+    n_workers = multiprocessing.cpu_count() - 1
+    chunksize, extra = divmod(dataset_size, n_workers * 4)  # good chunksize heuristic
+    if extra:
+        chunksize += 1
+    print("dataset_size: ", dataset_size)
+    print("n_workers: ", n_workers)
+    print("chunksize: ", chunksize)
+
+    with multiprocessing.Pool(n_workers) as P:
+        P.starmap(process_image, zip(enumerate(dataloader), repeat(features_dict)), chunksize=chunksize)
+        P.close()
+        P.join()
+
+    final_features = []
+    for i in range(len(features_dict)):
+        final_features.append(features_dict[i])
+
+    return pd.DataFrame(final_features)
+
+
+def test_reproducibility():
+    print("Testing reproducibility of feature extraction")
+    np.random.seed(42)
+    torch.manual_seed(42)
+    test_features_df = extract_features()
+    np.random.seed(42)
+    torch.manual_seed(42)
+    test_features_df_2 = extract_features()
+    print(test_features_df)
+    print(test_features_df_2)
+    print("Reproducible?", test_features_df.equals(test_features_df_2))
+
+
+if __name__ == "__main__":
+    TEST_REPROD = False
+
+    if TEST_REPROD:
+        test_reproducibility()
+
+    else:
+        np.random.seed(42)
+        torch.manual_seed(42)
+        final_features_df = extract_features()
+        final_features_df.to_csv('data/train_image_features.csv', index=True, header=False)
 
 
 # To display the image
