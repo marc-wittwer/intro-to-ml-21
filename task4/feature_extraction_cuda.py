520b6b5ec54921cd868ddda3aaf3e7afca7ec5c5
wenkf <wenkf@student.ethz.ch>
2021-05-31 16:51:12 +0200

    Baseline reached :)

--- task4/feature_extraction_cuda.py
+++ task4/feature_extraction_cuda.py
@@ -8,7 +8,7 @@ import time
 
 def extract_features():
     data_transform = transforms.Compose([
-        transforms.Resize(224),
+        transforms.Resize((350)),
         # transforms.CenterCrop(224),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
@@ -54,9 +54,9 @@ if __name__ == "__main__":
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-    extraction_net = models.mobilenet_v3_small(pretrained=True)
+    extraction_net = models.mobilenet_v3_small(pretrained=True, progress=True)
     extraction_net.to(device)
     extraction_net.eval()
 
     final_features_df = extract_features()
-    final_features_df.to_csv('data/image_features_mobileNetv3small.csv', index=True, header=False)
+    final_features_df.to_csv('data/image_features_mobileNetv3small350x350.csv', index=True, header=False)
